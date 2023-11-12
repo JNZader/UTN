5701fe1d95c5e2261a568b658a7bc27f6ab06e97
JNZader <javierzader@hotmail.com>
2023-11-12 20:14:42 -0300

    1er

---
+++ src/main/java/Actividad1/Inscripcion.java
@@ -0,0 +1,31 @@
+package Actividad1;
+
+import java.time.LocalDate;
+
+public class Inscripcion {
+    private Alumno alumno;
+    private Materia materia;
+    private LocalDate fecha;
+
+    public Inscripcion(Alumno alumno, Materia materia, LocalDate fecha) {
+        this.alumno = alumno;
+        this.materia = materia;
+        this.fecha = LocalDate.now();
+    }
+
+    public Alumno getAlumno() {
+        return alumno;
+    }
+
+    public void setAlumno(Alumno alumno) {
+        this.alumno = alumno;
+    }
+
+    public Materia getMateria() {
+        return materia;
+    }
+
+    public boolean aprobado(){
+        return !this.materia.tenesCorrelativas()||this.materia.puedeCursar(this.alumno);
+    }
+}
