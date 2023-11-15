e7fb76de7c3cf706b19aad6be1de023aba3c7f26
JNZader <javierzader@hotmail.com>
2023-11-15 01:27:49 -0300

    ejercicio 2 clase 3

--- src/main/java/Actividad3/Ej2.java
+++ src/main/java/Actividad3/Ej2.java
@@ -1,4 +1,23 @@
 package Actividad3;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Ej2 {
+    public static String concatenarPalabrasSLargo(List<String> palabras, int largo){
+        return palabras.stream().filter(palabra->palabra.length()>largo).collect(Collectors.joining(", "));
+    }
+
+    public static void main(String[] args) {
+        List<String> lista = new ArrayList<>();
+        lista.add("Hola");
+        lista.add("mundo");
+        lista.add("cruel");
+        lista.add("que se yo");
+        lista.add("hoy");
+
+        System.out.println(concatenarPalabrasSLargo(lista,4));
+
+    }
 }
